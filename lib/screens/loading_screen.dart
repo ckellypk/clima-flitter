6d8cfed02b78470d8fadba6445601c5df09df64c
ckellypk <ckelly@commure.com>
2022-01-25 19:00:46 -0500

    notes complete

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+
+/*
+* The geolocator is a widget that allows
+* Lifecycle methods allow us to inject actions at different times over the life
+* of the widget. initState() is one way to inject an action when the page loads.
+*
+* By taking it out of the build method we prevent it from lloading every time
+* the widget updates just when it loads initially.
+*
+* */
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +19,22 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    getLocation();
+  }
+
+  void getLocation() async {
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy:
+    LocationAccuracy.low);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
